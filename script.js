15413a5ae72c141344215a02c2fe8ae390593780
nicolej-amaral <nicole_amaral@estudante.sesisenai.org.br>
2026-03-17 08:39:07 -0300

    add: adicionei a primeira atividade

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ let peso = 58.0;
 
 console.log("Meu nome é " + nome);
 console.log("MInha idade é " + idade);
+
+function somarNumeros() {
+
+    let soma = 0;
+    let continuar = true;
+    while(continuar){
+        let numero = parseFloat (prompt("Digite um número(ou digite '0' para encerrar):"));
+
+        if(!isNaN(numero)) {
+
+            soma += numero;
+        } else {
+            alert("Por favor, digite um número válido.");
+        }
+
+        continuar = confirm("Deseja adicionar mais números?");
+
+        }
+
+        alert("A soma dos números é: " + soma);
+
+    }
+        somarNumeros();
