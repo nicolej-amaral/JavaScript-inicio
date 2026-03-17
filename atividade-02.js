8caf495429cc1e15221e7f5f13c97445f3caf656
nicolej-amaral <nicole_amaral@estudante.sesisenai.org.br>
2026-03-17 11:32:11 -0300

    add: atividade-02

---
+++ atividade-02.js
@@ -0,0 +1,13 @@
+function contagemRegressiva(){
+
+let regressiva = 1;
+while(regressiva < 10){
+    regressiva + 1;
+    alert("O foguete será lançado em: " + regressiva);
+    }
+
+    alert("Lançamento realizado!");
+
+};
+
+contagemRegressiva();
