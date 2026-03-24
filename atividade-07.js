039558795d4ce90b36d11bbc09d913161791f686
nicolej-amaral <nicole_amaral@estudante.sesisenai.org.br>
2026-03-24 09:14:07 -0300

    add: atividade-07

---
+++ atividade-07.js
@@ -0,0 +1,39 @@
+function picoleteria() {
+
+    alert("Escolha dentre as opções abaixo:");
+
+    let escolha = prompt("a Chocolate  \n b Morango  \n c Creme  \n d Manga  \n e Melância  \n f Vanilla Ice  \n g Céu Azul  \n h Brownie  \n i Hawaiano ");
+
+    if (escolha == "a") {
+        alert("Você escolheu o sabor chocolate que custa R$1,50");
+
+
+    } else if (escolha == "b") {
+        alert("Você escolheu morango que custa R$2,50");
+
+    } else if (escolha == "c") {
+        alert("Você escolheu o sabor creme que custa R$2,50");
+
+    } else if (escolha == "d") {
+        alert("Você escolheu o sabor manga que custa R$3,20");
+
+    } else if (escolha == "e") {
+        alert("Você escolheu o sabor melância que custa R$3,40");
+
+    } else if (escolha == "f") {
+        alert("Você escolheu o sabor Vanilla Ice que custa R$3,00");
+
+    } else if (escolha == "g") {
+        alert("Você escolheu o sabor Céu Azul que custa R$3,60");
+
+    } else if (escolha == "h") {
+        alert("Você escolheu o sabor Brownie que custa R$4,00");
+
+    } else (escolha == "i"); {
+        alert("Você escolheu o sabor Hawaiano que custa R$5,00");
+
+    }
+
+    }
+
+
